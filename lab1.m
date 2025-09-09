cbbc7cb8995c1b2261ff4aef1f983e3cdf131ac7
Vallis-LFC <115032819+Vallis-LFC@users.noreply.github.com>
2025-09-09 17:40:18 -0400

    Add files via upload

--- lab1.m
+++ lab1.m
@@ -1,7 +1,30 @@
+%A.1
+
 t = (-2:2);  %from -2 to 2
 f = @(t) exp(-t).*cos(2*pi*t);  %f(t)
 plot(t,f(t)); 
+xlabel('t');
+ylabel('f(t)');
+grid;
 
 t = (-2:0.01:2); %from -2 to 2 in increments of 0.01
 f = @(t) exp(-t).*cos(2*pi*t); %f(t)
 plot(t,f(t));
+xlabel('t');
+ylabel('f(t)');
+grid;
+
+%A.2
+
+t = (-2:2);
+f = @(t) exp(-t);
+plot(t, f(t));
+xlabel('t');
+ylabel('f(t)');
+grid;
+
+%A.3
+%they are identitcal due to the large step size that was set for these 2
+%graphs. All the graphs are decreasing exponentially.
+
+%B.1
