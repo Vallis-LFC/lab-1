e1dae3bacbddee54f2dfcbbf7ed9f9adab535c8d
Vallis-LFC <115032819+Vallis-LFC@users.noreply.github.com>
2025-09-10 00:06:59 -0400

    up to b.3 but b stuff not tested yet

--- lab1.m
+++ lab1.m
@@ -12,6 +12,7 @@ f = @(t) exp(-t).*cos(2*pi*t); %f(t)
 plot(t,f(t));
 xlabel('t');
 ylabel('f(t)');
+yticks([-5 0 5 10]);
 grid;
 
 %A.2
@@ -36,4 +37,22 @@ plot(t,p(t));
 xlabel('t');
 ylabel('p(t)');
 axis([-1 2 -.1 1.1])
+yticks([0 0.5 1]);
 grid;
+
+%B.2        not tested yet
+t = (-1:0.01:2);  %from -2 to 2
+u = @(t) 1.0.*((-1<=t)&(t<=2)); %makes unit step function
+p = @(t) 1.0.*((t>=0)&(t<1));  %f(t)
+r = @(t) t.*p(t);
+n = @(t) r(t)+r(-t+2);
+
+plot(t,p(t)); 
+xlabel('t');
+ylabel('r(t)');
+axis([-1 2 -.1 1.1])
+yticks([0 0.5 1]);
+grid;
+
+%B.3
+n1 = @(t) n(t+0.25);
